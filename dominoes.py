f06817e35831449924340da6cd21a55e9881523a
Marcus Cesar <marcuscesar1996@gmail.com>
2020-05-02 21:59:11 -0500

    Created classes

--- dominoes.py
+++ dominoes.py
@@ -47,34 +47,26 @@ class Tile(object):
         return not self == other
 
 
-# t = Tile(End(1), End(2))
-# t2 = Tile(End(1), End(2))
-
-# if t == t2:
-#     t.print()
-
 class TileList(object):
-    def __init__(self):
-        self.list = []
-
-    def setList(self, l):
+    def __init__(self, l=[]):
         self.list = l
-
-    def append(self, t):
-        self.list.append(t)
 
     def getList(self):
         return self.list
-
-    def remove(self, t):
-        self.list.remove(t)
 
     def isEmpty(self):
         return not self.list
 
     def print(self):
         for t in self.list:
-            t.print()
+            t.end1.print()
+            print(" ", end='')
+
+        print()
+
+        for t in self.list:
+            t.end2.print()
+            print(" ", end='')
 
 
 class Deck(TileList):
@@ -84,120 +76,31 @@ class Deck(TileList):
         # create tiles and add them to deck
         for i in range(7):
             for j in range(i, 7):
-                self.list.append(Tile(End(i), End(j)))
+                t = Tile(End(i), End(j))
+                self.list.append(t)
 
     def shuffle(self):
         random.shuffle(self.list)
 
+    def distributeHand(self):
+        if self.isEmpty():
+            raise Exception("All tiles have been distributed")
 
-d = Deck()
-d.print()
-# class Board(object):
-#     def __init__(self):
-#         self.deck = []
-#         self.board = []
+        hand = Hand()
 
-#         # create tiles and add them to deck
-#         for i in range(7):
-#             for j in range(i, 7):
-#                 self.deck.append(Tile(i, j))
+        # takes tiles from deck and place in hand
+        for i in range(7):
+            hand.list.append(self.list.pop())
 
-#     def shuffleDeck(self):
-#         random.shuffle(self.deck)
+        return hand
 
-#     def getHand(self):
-#         if not self.deck:
-#             raise Exception("All tiles have been distributed")
-
-#         # create hand with 7 tiles, passes in board ends
-#         hand = Hand(self.deck[0:7], self.deck[0].end1,
-#                     self.deck[len(self.deck) - 1].end1)
-
-#         del self.deck[0:7]  # remove hand from deck
-#         return hand
-
-#         # self.board.append(h1.getTile()) #
-
-#     def print(self):
-#         for tile in self.board:
-#             tile.print()
-
-#     def getDeck(self):
-#         return self.deck
-
-#     def getBoard(self):
-#         return self.board
+    def print(self):
+        # this overrides the TileList print function
+        print()
 
 
-# class Hand(object):
-#     def __init__(self, h, t1, t2):
-#         self.hand = h
-#         self.boardEnd1 = t1
-#         self.boardEnd2 = t2
-
-#     def isValid(self, tile):
-#         # if the board is empty
-#         if not self.boardEnd1:
-#             return True
-
-#         if (tile.end1 is t1 or tile.end2 is t1
-#                 or tile.end1 is t2 or tile.end2 is t2):
-#             return True
-
-#         # if ntile.end1 is t1 or tile.end2 is t1
-#         #     or tile.end2 is t1 or tile.end2 is t2):
-#         #     return True
-
-#         return False
-
-#     def placeTile(self, t):
-#         if t not in self.hand:
-#             print("That tile is not in your hand.\n")
-#             return
-
-#         if not self.isValid(t):
-#             print("That tile can not be placed on the board.\n")
-#             return
-
-#         for tile in self.hand:
-#             if tile == t:
-#                 self.hand.remove(tile)
-#                 return t
-
-#     def print(self):
-#         for tile in self.hand:
-#             tile.printEnd1()
-#             print(" ", end='')
-
-#         print()
-
-#         for tile in self.hand:
-#             tile.printEnd2()
-#             print(" ", end='')
-#             # for i in range(0, 7):
-
-
-# b = Board()
-# # b.shuffleDeck()
-
-# playerHand = b.getHand()
-# computerHand = b.getHand()
-
-# tile = Tile(0, 0)
-# playerHand.placeTile(tile)
-
-# print("Player: \n")
-# playerHand.print()
-
-# print("\n\n")
-# b.print()
-
-# e = End(1)
-# print(end)
-# # print("CPU: \n")
-# # computerHand.print()
-
-
-# # b.print()
-
-print()
+class Hand(TileList):
+    def __init__(self):
+        super().__init__()
+        self.list = []
+        pass
